94561459f4f7f8ccd81a5e3b745df26aaee1c6a5
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2021-01-05 11:48:26 -0700

    worked on adding theming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React from 'react';
+
+import {Paper, Switch} from '@material-ui/core';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: "dark"
+    }
+  })
+  
+  const lightTheme = createMuiTheme({
+    palette: {
+      type: "light"
+    }  
+  });
+
   return (
-    <h1>
-      hello
-    </h1>
-  );
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}> 
+        <Paper square={true} style={{height: "100vh"}}>
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Paper>
+      </ThemeProvider>
+  )
 }
+
+
 
 export default App;
